38c0c09763312e69bb92d4feb4b018e536e75224
Qays12 <qays_786@hotmail.co.uk>
2023-10-29 11:45:14 +0000

    Add files via upload

--- AutoMudaeRoller.py
+++ AutoMudaeRoller.py
@@ -3,13 +3,13 @@ import time
 from configparser import ConfigParser
 
 
-def roll(rolls, command):
+def roll(rolls, command, time_inbetween_rolls):
     i = 1
     while i <= rolls:
         line = command
         pyautogui.typewrite(line)
         pyautogui.press("enter")
-        time.sleep(1)
+        time.sleep(time_inbetween_rolls)
         i += 1
 
 
@@ -25,7 +25,8 @@ Type 2 If you want to load a save data
             input("How many rolls do you want to roll for (give answer in numbers only): "))
         command = input("What command do you want to type? ")
         secs = int(input(
-            "How many seconds do you want before the program runs, so you have enough time to go to discord: "))
+            "How many seconds do you want before the program runs, so you have enough time to go to discord (give answer in numbers only): "))
+        time_inbetween_rolls = int(input("How many seconds do you want for each roll to run for (give answer in numbers only): "))
         print(
             f"You Have {secs} Secs Before The Program Runs Go To Discord Immediately!")
 
@@ -33,14 +34,15 @@ Type 2 If you want to load a save data
         config_object["USERINFO"] = {
             "rolls": rolls,
             "command": command,
-            "secs": secs
+            "secs": secs,
+            "time_inbetween_rolls": time_inbetween_rolls, 
         }
 
         with open('AutoMudaeRollerconfig.ini', 'w') as conf:
             config_object.write(conf)
 
             time.sleep(secs)
-            roll(rolls, command)
+            roll(rolls, command, time_inbetween_rolls)
 
     elif savedata == 2:
         config_object = ConfigParser()
@@ -49,12 +51,13 @@ Type 2 If you want to load a save data
         rolls = int(userinfo["rolls"])
         command = userinfo["command"]
         secs = int(userinfo["secs"])
+        time_inbetween_rolls = int(userinfo["time_inbetween_rolls"])
 
         print(
             f"You Have {secs} Secs Before The Program Runs Go To Discord Immediately!")
 
         time.sleep(secs)
-        roll(rolls, command)
+        roll(rolls, command, time_inbetween_rolls)
 
 
 mudae()
